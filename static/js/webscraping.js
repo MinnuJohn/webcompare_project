d251d5c8846282efa6765c25cef3fa16dba0ece9
Minnu John <minnu.john@snhu.edu>
2023-09-03 06:43:31 -0500

    js file created

---
+++ static/js/webscraping.js
@@ -0,0 +1,13 @@
+function checkPasswordMatch() {
+    var password = document.getElementById("psw").value;
+    var confirmPassword = document.getElementById("psw-repeat").value;
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return false; // Prevent form submission
+    }
+
+    return true; // Allow form submission
+  }
+
+ 
