5b83f97711b927811dcb212b053bc2e68aa3f143
Minnu John <minnu.john@snhu.edu>
2023-09-21 17:49:57 -0500

    working on back button on result page

---
+++ static/js/registerform.js
@@ -0,0 +1,14 @@
+function checkPasswordMatch() {
+    var password = document.getElementById("psw").value;
+    var confirmPassword = document.getElementById("psw-repeat").value;
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return false; // Prevent form submission
+    }
+
+    return true; // Allow form submission
+  }
+
+
+
